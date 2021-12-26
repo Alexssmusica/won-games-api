265bfe285211305ef324978c2c5816337d8156d3
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2021-12-26 13:42:07 -0300

    node mail

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -1,14 +1,22 @@
 module.exports = ({ env }) => ({
-    graphql: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: false,
-        depthLimit: 7,
-        amountLimit: 10000,
-        disabledPlugins: [],
-        disabledExtensions: [],
-        apolloServer: {
-            tracing: true,
-        },
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 10000,
+    disabledPlugins: [],
+    disabledExtensions: [],
+    apolloServer: {
+      tracing: true,
     },
+  },
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: "localhost",
+      port: 1025,
+      ignoreTLS: true,
+    },
+  },
 })
